5c0d54fcb0192762b8b769ea6f549267769b0b4e
Felipe Brum <fbrump@gmail.com>
2016-02-13 16:59:25 -0200

    First execise about types in Python

---
+++ week-4/execise-2_2-types.py
@@ -0,0 +1,10 @@
+# Exercise:
+# Input information
+hours = raw_input('Enter hours: ')
+rate = raw_input('Enter rate: ')
+pay = float(hours) * float(rate)
+
+# Output results
+print('Enter Hours: ' + hours)
+print('Enter Rate: ' + rate)
+print('Pay: ' + str(pay))
